cf7b46a8d4ac46dd9d3580886b3f9f20cf3530c1
A Vamsi Mudaliar <summersunshine360@gmail.com>
2021-04-02 14:05:11 +0530

    added Article Page

--- sample-react-app/src/components/articles/Articles.js
+++ sample-react-app/src/components/articles/Articles.js
@@ -1,9 +1,25 @@
 import React from 'react'
-
+import '../../App.css'
+import './Articles.css'
 export const Articles = () => {
     return (
-        <div>
-            <h3>This is Article Page</h3>
+        <div className="">
+
+            <div className="article-layout d-flex flex-lg-row 
+            flex-sm-column align-items-sm-center flex-wrap justify-content-space-between">
+                
+                
+               <div class="card d-flex flex-column mx-5 ">
+
+                        <img className="img img-reponsive card-image mx-auto my-3" src="https://picsum.photos/200/300?random=1"/>
+
+                        <div className="card-content pb-3">
+                            <h3 className="text-center">First Blog Post</h3>
+                            <p className="card-text mt-4 pl-3">lkdlkfmlfmldmvlsdkmvsv</p>
+                        </div>            
+                        <button className="btn btn-style btn-lg">Read More</button>
+                </div>
+              </div>
         </div>
     )
 }
